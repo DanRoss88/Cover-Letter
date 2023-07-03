942c49a4b2ffc2a81122400990e58dad8bd40497
DanRoss88 <danielmartinross@gmail.com>
2023-07-02 21:28:41 -0700

    lots of movement now, it looks better, still lots of work on negative space and image

--- cover-letter/pages/components/Company.js
+++ cover-letter/pages/components/Company.js
@@ -1,10 +1,39 @@
+import { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 export default function Company() {
+  const cardVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 },
+  };
+
+  const controls = useAnimation();
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.2,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
   return (
     <div className="company-container">
       <h1>What I love about the Company</h1>
       <span>Here are some facts that I really like about the company.</span>
       <div className="company-grid">
-        <div className="company-card">
+        <motion.div
+          className="company-card"
+          ref={ref}
+          initial="hidden"
+          animate={controls}
+          variants={cardVariants}
+          transition={{ duration: 0.5 }}
+        >
+          <div className="company-logo1">
           <h2>Company Fact 1</h2>
           <p>
             Here is a fact about the company that I really like. Here is a fact
@@ -16,8 +45,16 @@ export default function Company() {
             company that I really like. Here is a fact about the company that I
             really like.
           </p>
-        </div>
-        <div className="company-card">
+          </div>
+        </motion.div>
+        <motion.div
+          className="company-card"
+          initial="hidden"
+          animate={controls}
+          variants={cardVariants}
+          transition={{ duration: 0.5, delay: 0.1 }}
+        >
+          <div className="company-logo2">
           <h2>Company Fact 2</h2>
           <p>
             Here is a fact about the company that I really like. Here is a fact
@@ -29,8 +66,16 @@ export default function Company() {
             company that I really like. Here is a fact about the company that I
             really like.
           </p>
-        </div>
-        <div className="company-card">
+          </div>
+        </motion.div>
+        <motion.div
+          className="company-card"
+          initial="hidden"
+          animate={controls}
+          variants={cardVariants}
+          transition={{ duration: 0.5, delay: 0.2 }}
+        >
+          <div className="company-logo3">
           <h2>Company Fact 3</h2>
           <p>
             Here is a fact about the company that I really like. Here is a fact
@@ -42,7 +87,8 @@ export default function Company() {
             company that I really like. Here is a fact about the company that I
             really like.
           </p>
-        </div>
+          </div>
+        </motion.div>
       </div>
     </div>
   );
--- cover-letter/pages/components/WhyMe.js
+++ cover-letter/pages/components/WhyMe.js
@@ -1,42 +1,73 @@
+import { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
 export default function WhyMe() {
+  const controls = useAnimation();
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.2,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+  }, [controls, inView]);
+
+  const cardVariants = {
+    visible: { opacity: 1, y: 0 },
+    hidden: { opacity: 0, y: 50 },
+  };
+
   return (
     <div className="whyme-container">
       <h1>Why Me?</h1>
       <span>Here are some reasons why I would be a great fit for the role</span>
       <div className="whyme-grid">
-        <div className="whyme-card">
-          <h2>Reason 1</h2>
-          <p>
-            Here is a reason why I would be a great fit for the role. Here is a
-            reason why I would be a great fit for the role. Here is a reason why
-            I would be a great fit for the role. Here is a reason why I would be
-            a great fit for the role. Here is a reason why I would be a great
-            fit for the role. Here is a reason why I would be a great fit for
-            the role.
-          </p>
-        </div>
-        <div className="whyme-card">
-          <h2>Reason 2</h2>
-          <p>
-            Here is a reason why I would be a great fit for the role. Here is a
-            reason why I would be a great fit for the role. Here is a reason why
-            I would be a great fit for the role. Here is a reason why I would be
-            a great fit for the role. Here is a reason why I would be a great
-            fit for the role. Here is a reason why I would be a great fit for
-            the role. Here is a reason why I would be a great fit for the role.
-          </p>
-        </div>
-        <div className="whyme-card">
-          <h2>Reason 3</h2>
-          <p>
-            Here is a reason why I would be a great fit for the role. Here is a
-            reason why I would be a great fit for the role. Here is a reason why
-            I would be a great fit for the role. Here is a reason why I would be
-            a great fit for the role. Here is a reason why I would be a great
-            fit for the role. Here is a reason why I would be a great fit for
-            the role. Here is a reason why I would be a great fit for the role.
-          </p>
-        </div>
+        <motion.div
+          className="whyme-card why-me1"
+          ref={ref}
+          initial="hidden"
+          animate={controls}
+          variants={cardVariants}
+          transition={{ duration: 0.5 }}
+        >
+          <h2>Reason 1: Quickly Adopt and Learn</h2>
+          <motion.p>
+            I pride myself on my ability to quickly learn and adapt to new technologies. I am always curious to learn new technologies!
+            One of the things I love about software development is that it's a never-ending learning journey, and I am always looking for ways to expand my skills and knowledge in the field.
+          </motion.p>
+        </motion.div>
+        <motion.div
+          className="whyme-card why-me2"
+          ref={ref}
+          initial="hidden"
+          animate={controls}
+          variants={cardVariants}
+          transition={{ duration: 0.5, delay: 0.1 }}
+        >
+          <h2>Reason 2: Collaboration</h2>
+          <motion.p>
+            I believe that teamwork is key to achieving success, and I am committed to doing my part to contribute to the Company team's efforts.
+            Empathy and kindness are values that are important to me, and I try to incorporate them into my interactions with others.
+          </motion.p>
+        </motion.div>
+        <motion.div
+          className="whyme-card why-me3"
+          ref={ref}
+          initial="hidden"
+          animate={controls}
+          variants={cardVariants}
+          transition={{ duration: 0.5, delay: 0.2 }}
+        >
+          <h2>Reason 3: Passionate</h2>
+          <motion.p>
+            I have a strong desire to create software and online experiences that are both visually appealing and intuitive for users.
+            I am always looking for ways to make the user's journey through a website as seamless and enjoyable as possible.
+            I believe that design can change the way people interact with software.
+          </motion.p>
+        </motion.div>
       </div>
     </div>
   );
